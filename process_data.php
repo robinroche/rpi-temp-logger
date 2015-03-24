98259c82c3a5667ade61835347d48e14da2d3700
Robin Roche <robin.roche@gmail.com>
2015-03-24 12:48:37 +0100

    Update process_data.php

--- process_data.php
+++ process_data.php
@@ -1,6 +1,7 @@
 <?php
+
 // Connexion MySQL
-$serveur="xxxx";
+$serveur="xxxx"; 
 $login="xxxx";
 $base="xxxx";
 $pass="xxxx";
@@ -8,26 +9,29 @@ $table="xxxx";
 
 // Verification mot de passe
 if(isset($_GET["PASSWD"])) {
- $passwd = $_GET["PASSWD"];
- $realPasswd = "123456";
- if(strcmp($passwd,$realPasswd)==0) {
-  // Recuperation temperature
-  if(!isset($_GET["TEMPERATURE"])) {
-   $temp_c= "NULL";
-  }else {
-   $temp_c= $_GET["TEMPERATURE"];
-  }
-  
-  // Connexion au serveur
-  $con = mysql_connect($serveur, $login, $pass);
-  if (!$con)
-   {
-    die('Could not connect: ' . mysql_error());
-   }
-  mysql_select_db($base) or die("Erreur de connexion a la base de donnees $base");
-  mysql_query("SET NAMES 'utf8'");
-  // Ajout des donnees dans la base
-  $requete = mysql_query("INSERT INTO $table(time,temperature) VALUES(NOW(),$temp_c)");
- }
+	$passwd = $_GET["PASSWD"];
+	$realPasswd = "123456";
+	
+	if(strcmp($passwd,$realPasswd)==0) {
+		// Recuperation temperature
+		if(!isset($_GET["TEMPERATURE"])) {
+			$temp_c= "NULL";
+		}else {
+			$temp_c= $_GET["TEMPERATURE"];
+		}
+
+		// Connexion au serveur
+		$con = mysql_connect($serveur, $login, $pass);
+		if (!$con)
+		  {
+		  die('Could not connect: ' . mysql_error());
+		  }
+
+		mysql_select_db($base) or die("Erreur de connexion a la base de donnees $base");
+		mysql_query("SET NAMES 'utf8'");
+
+		// Ajout des donnees dans la base
+		$requete = mysql_query("INSERT INTO $table (time,temperature) VALUES(NOW(),$temp_c)");
+	}
 }
 ?>
